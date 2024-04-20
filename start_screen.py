e6cff90b91da443bb36510bb031ea53804661570
Nicholas Wilson <56737727+Mial42@users.noreply.github.com>
2024-04-20 19:11:51 -0400

    Update start_screen.py

--- start_screen.py
+++ start_screen.py
@@ -5,9 +5,22 @@ class StartScreen(Screen):
     def __init__(self, screen:pygame.display, bg_path:str, screen_switch_event_val:int) -> None:
         super().__init__(screen, bg_path, screen_switch_event_val)
     
-        self.start_rct = pygame.Rect(pygame.display.get_window_size()[0] / 2 - 50, pygame.display.get_window_size()[1] / 2 - 50, 100, 100)
-    
-        self.quit_rct = pygame.Rect(pygame.display.get_window_size()[0] / 2 - 50, pygame.display.get_window_size()[1] / 2 + 100, 100, 100)
+ # Define button colors
+        self.start_color = (0, 255, 0)  # Green
+        self.quit_color = (255, 0, 0)   # Red
+        
+        # Define button positions and dimensions
+        window_width, window_height = pygame.display.get_window_size()
+        button_width = 100
+        button_height = 50
+        button_spacing = 20
+        button_x = (window_width - button_width) // 2
+        start_button_y = (window_height - (2 * button_height + button_spacing)) // 2
+        quit_button_y = start_button_y + button_height + button_spacing
+        
+        # Create button rectangles
+        self.start_rct = pygame.Rect(button_x, start_button_y, button_width, button_height)
+        self.quit_rct = pygame.Rect(button_x, quit_button_y, button_width, button_height)
 
     def handle_event(self, event):
         if self.start_rct.collidepoint(event.pos):
@@ -15,5 +28,19 @@ class StartScreen(Screen):
         elif self.quit_rct.collidepoint(event.pos):
             super().quit()
         
+    def draw(self):
+        # Draw background
+        super().draw()
+        
+        # Draw buttons
+        pygame.draw.rect(self.screen, self.start_color, self.start_rct)
+        pygame.draw.rect(self.screen, self.quit_color, self.quit_rct)
+        
+        # Add text to buttons
+        font = pygame.font.Font(None, 36)
+        start_text = font.render("Start", True, (255, 255, 255))  # White text
+        quit_text = font.render("Quit", True, (255, 255, 255))    # White text
+        self.screen.blit(start_text, (self.start_rct.x + 20, self.start_rct.y + 10))
+        self.screen.blit(quit_text, (self.quit_rct.x + 30, self.quit_rct.y + 10))
 
     
