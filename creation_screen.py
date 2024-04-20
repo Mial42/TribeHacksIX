90cb4c34f1ac9fd896267e6d71954e23b88e33cb
Nicholas Wilson <56737727+Mial42@users.noreply.github.com>
2024-04-20 19:06:06 -0400

    Update creation_screen.py

--- creation_screen.py
+++ creation_screen.py
@@ -2,7 +2,7 @@ from screen import Screen
 import pygame
 
 class CreationScreen(Screen):
-    def __init__(self, screen, bg_path:str, screen_switch_event_val:int, currentlegs: str, currenthead: str, currentbody: str, headlist: list, legslist:list, bodylist: list) -> None:
+    def __init__(self, screen, bg_path:str, screen_switch_event_val:int) -> None:
         super().__init__(screen=screen, bg_path=bg_path, screen_switch_event_val=screen_switch_event_val)
         self.setup_head_next_button()
         self.setup_head_prev_button()
@@ -11,9 +11,9 @@ class CreationScreen(Screen):
         self.headlist = ["head1.png", "head2.png","clawdius.png"]
         self.bodylist = ["body1.png", "body2.png"]
         self.legslist = ["legs1.png", "legs2.png"]
-        self.currenthead = headlist[0]
-        self.currentbody = bodylist[0]
-        self.currentlegs = legslist[0]
+        self.currenthead = self.headlist[0]
+        self.currentbody = self.bodylist[0]
+        self.currentlegs = self.legslist[0]
     def setup_head_next_button(self):
         self.button_color = (0, 128, 0)  # Green button
         self.button_rect = pygame.Rect(300, 300, 200, 50)  # Button dimensions and position
