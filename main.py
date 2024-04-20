1f32baab09f5a08d3dc541ada79d87c0f60ac47f
Kyle Chen <78221899+K-L-Chen@users.noreply.github.com>
2024-04-20 12:27:01 -0400

    setup main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame import mixer
+#import sys
+
+def run():
+    return 0
+
+def main():
+    mixer.init()
+    pygame.init()
+
+    screen_width = 1080
+    screen_height = 600
+
+    screen = pygame.display.set_mode(screen_width, screen_height)
+    pygame.display.set_caption("Mecha Fighter!")
+    
+    
+
+if __name__ == "__main__":
+    main()
