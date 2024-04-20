af3ccf95d96255edd88cb2f7be6ad745296c1591
Kyle Chen <78221899+K-L-Chen@users.noreply.github.com>
2024-04-20 13:39:55 -0400

    fix display

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     screen_width = 1080
     screen_height = 600
 
-    screen = pygame.display.set_mode(screen_width, screen_height)
+    screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Mecha Fighter!")
     
     
