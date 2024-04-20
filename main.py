5c214cd20511402ca6c0ad83f57dc24e73bbdd1f
Kyle Chen <78221899+K-L-Chen@users.noreply.github.com>
2024-04-20 14:02:39 -0400

    Dummy Code for testing PyGame

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 from pygame import mixer
 #import sys
+import os
+
+#Screens
+import start_screen
+import creation_screen
+import fighting_screen
+import game_over_screen
 
 def run():
     return 0
@@ -14,6 +21,40 @@ def main():
 
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Mecha Fighter!")
+    # Run until the user asks to quit
+
+    running = True
+
+    while running:
+
+
+        # Did the user click the window close button?
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+
+                running = False
+
+
+        # Fill the background with white
+
+        screen.fill((255, 255, 255))
+
+
+        # Draw a solid blue circle in the center
+
+        pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+
+        # Flip the display
+
+        pygame.display.flip()
+
+
+    # Done! Time to quit.
+
+    pygame.quit()
     
     
 
---
+++ screen.py
@@ -0,0 +1,14 @@
+import pygame
+import os
+
+class Screen:
+
+    def __init__(self, pygame_game:pygame, bg_path:str, screen_width:int = 1080, screen_height = 600) -> None:
+        self.__bg_path = bg_path
+        self.__screen_width = screen_width
+        self.__screen_height = screen_height
+
+    def draw_bg(self):
+        pygame.
+        return
+
