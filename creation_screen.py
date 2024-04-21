63967bab7cdeaa41d4a5a347a32bb50ef0e0bff0
Sam <samuel.borak@gmail.com>
2024-04-20 23:29:20 -0400

    Possibly done with creation

--- creation_screen.py
+++ creation_screen.py
@@ -25,7 +25,7 @@ class CreationScreen(Screen):
         #text setup
         start_text = self.font.render("Start", True, (255, 255, 255))  # White text
         self.screen_hold.blit(start_text, (self.start_rct.x + 20, self.start_rct.y + 10))
-        self.setup_start_button()
+
         self.headlist = ["assets/imgs/head1.png", "assets/imgs/head2.png","assets/imgs/clawdius.png"]
         self.bodylist = ["assets/imgs/body1.png", "assets/imgs/body2.png"]
         self.legslist = ["assets/imgs/legs1.png", "assets/imgs/legs2.png"]
@@ -33,25 +33,47 @@ class CreationScreen(Screen):
         self.currentbody = 1
         self.currentlegs = 1
        
-    def handle_start_button_event(self, event):
-        if self.button_rect.collidepoint(event.pos):
+    def handle_event(self, event):
+        if self.start_button.collidepoint(event.pos):
             # return 'start_screen'  #Need to switch to start
             num = self.currenthead*100 + self.currentbody*10 + self.currentlegs
             super().switch_screen(num)
-    def setup_start_button(self):
-        self.button_color = (0, 128, 0)  # Green button
-        self.button_start = pygame.Rect(300, 300, 200, 50)  # Button dimensions and position
-    def draw_button(self, text,part,index):
+        elif self.head_prev_button.collidepoint(event.pos):
+            if(self.currenthead != 1):
+                self.currenthead -= 1
+                self.draw_image(self, 1)
+        elif self.head_next_button.collidepoint(event.pos):
+            if(self.currenthead != 3):
+                self.currenthead += 1
+                self.draw_image(self, 1)
+        elif self.body_prev_button.collidepoint(event.pos):
+            if(self.currentbody != 1):
+                self.currentbody -= 1
+                self.draw_image(self, 2)
+        elif self.body_next_button.collidepoint(event.pos):
+            if(self.currentbody != 3):
+                self.currentbody += 1
+                self.draw_image(self, 2)
+        elif self.legs_prev_button.collidepoint(event.pos):
+            if(self.currentlegs != 1):
+                self.currentlegs -= 1
+                self.draw_image(self, 3)
+        elif self.legs_next_button.collidepoint(event.pos):
+            if(self.currentlegs != 3):
+                self.currentlegs += 1
+                self.draw_image(self, 3)
+    def draw_button(self, text):
         pygame.draw.rect(super().__screen, self.button_color, self.button_rect)  # Draw the button
         text_surface = self.font.render(text, True, (255, 255, 255))
         text_rect = text_surface.get_rect(center=self.button_rect.center)
         super().get_screen().blit(text_surface, text_rect)
+    def draw_image(self, part):
         if(part == 1):
-            img_scaled = pygame.transform.scale(pygame.image.load(self.headlist[index]).convert_alpha(), 200, 400)
+            img_scaled = pygame.transform.scale(pygame.image.load(self.headlist[self.currenthead-1]).convert_alpha(), 200, 400)
             super().get_screen().blit(img_scaled, (150, 450))
         elif(part ==2):
-            img_scaled = pygame.transform.scale(pygame.image.load(self.bodylist[index]).convert_alpha(), 200, 400)
+            img_scaled = pygame.transform.scale(pygame.image.load(self.bodylist[self.currentbody-1]).convert_alpha(), 200, 400)
             super().get_screen().blit(img_scaled, (150, 250))
         else:
-            img_scaled = pygame.transform.scale(pygame.image.load(self.legslist[index]).convert_alpha(), 200, 400)
+            img_scaled = pygame.transform.scale(pygame.image.load(self.legslist[self.currentlegs-1]).convert_alpha(), 200, 400)
             super().get_screen().blit(img_scaled, (150, 50))
