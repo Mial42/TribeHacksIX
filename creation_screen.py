d2e19636af6fe3877003708cffc51b1e8c985c4e
Sam <samuel.borak@gmail.com>
2024-04-20 22:09:10 -0400

    Creation changes again

--- creation_screen.py
+++ creation_screen.py
@@ -2,7 +2,7 @@ from screen import Screen
 import pygame
 
 class CreationScreen(Screen):
-    def __init__(self, screen, bg_path:str, screen_switch_event_val:int) -> None:
+    def __init__(self, screen, bg_path:str, screen_switch_event_val:int, int currenthead, int currentbody, int currentlegs) -> None:
         super().__init__(screen=screen, bg_path=bg_path, screen_switch_event_val=screen_switch_event_val)
         self.setup_mech_button()
         self.setup_mech_button()
@@ -15,9 +15,9 @@ class CreationScreen(Screen):
         self.headlist = ["head1.png", "head2.png","clawdius.png"]
         self.bodylist = ["body1.png", "body2.png"]
         self.legslist = ["legs1.png", "legs2.png"]
-        self.currenthead = 0
-        self.currentbody = 0
-        self.currentlegs = 0
+        self.currenthead = 1
+        self.currentbody = 1
+        self.currentlegs = 1
     def setup_mech_button(self, x, y):
         self.button_color = (0, 0, 128)  # Blue button
         self.button_mech = pygame.Rect(x, y, 200, 50)  # Button dimensions and position
@@ -25,7 +25,8 @@ class CreationScreen(Screen):
     def handle_start_button_event(self, event):
         if self.button_rect.collidepoint(event.pos):
             # return 'start_screen'  #Need to switch to start
-            super().switch_screen(1)
+            str num = currenthead + currentbody + currentlegs
+            super().switch_screen()
     def setup_start_button(self):
         self.button_color = (0, 128, 0)  # Green button
         self.button_start = pygame.Rect(300, 300, 200, 50)  # Button dimensions and position
