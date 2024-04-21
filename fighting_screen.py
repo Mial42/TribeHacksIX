28e20caec8756bd2e996d2986fc31ce193ed4901
Kyle Chen <78221899+K-L-Chen@users.noreply.github.com>
2024-04-20 21:40:24 -0400

    Moved images to proper asset folder, fixed image loading issue in fighting_screen.py

--- fighting_screen.py
+++ fighting_screen.py
@@ -13,7 +13,7 @@ class FightingScreen(Screen):
         self.__final_score = 0
     
     def spawn_enemies(self):
-        num_enemies = [Enemy(random.randint(1, 30), random.randint(1, 30), random.randint(1, 30), random.randint(1, 30), random.randint(1, 30), )]random.randint(0,11)
+        num_enemies = [Enemy(random.randint(1, 30), random.randint(1, 30), random.randint(1, 30), random.randint(1, 30), random.randint(1, 30), os.path.join("./assets/imgs/enemy/hammer.png").replace(path_separator[system()][0], path_separator[system()][1]))]random.randint(0,11)
         
         return
 
@@ -32,10 +32,10 @@ class Enemy:
         #limit speed by however large the enemy is
         self.__speed = (lambda s: s if s < self.__sizeX else self.__sizeX - 10)(speed)
 
-        img_scaled = pygame.transform.scale(pygame.image.load(os.path.join(self.__imgpath).replace(path_separator[system()][0]).convert_alpha(), self.__sizeX, self.__sizeY))
+        img_scaled = pygame.transform.scale(pygame.image.load(os.path.join(self.__imgpath).replace(path_separator[system()][0], path_separator[system()][1]).convert_alpha(), self.__sizeX, self.__sizeY))
         super().get_screen.blit(img_scaled, (self.__sizeX, self.__sizeY))
 
     def update_pos(self):
         self.__posX -= self.__speed
-        img_scaled = pygame.transform.scale(pygame.image.load(os.path.join(self.__imgpath).replace(path_separator[system()][0]).convert_alpha(), self.__sizeX, self.__sizeY))
+        img_scaled = pygame.transform.scale(pygame.image.load(os.path.join(self.__imgpath).replace(path_separator[system()][0], path_separator[system()][1]).convert_alpha(), self.__sizeX, self.__sizeY))
         super().get_screen.blit(img_scaled, (self.__posX, self.__posY))
