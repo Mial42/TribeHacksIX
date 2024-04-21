bc9d2eefdb36a2738727efb2d39aa020a30bd7b8
Sam <samuel.borak@gmail.com>
2024-04-20 21:06:55 -0400

    made creation_screen better again

--- creation_screen.py
+++ creation_screen.py
@@ -5,6 +5,7 @@ class CreationScreen(Screen):
     def __init__(self, screen, bg_path:str, screen_switch_event_val:int) -> None:
         super().__init__(screen=screen, bg_path=bg_path, screen_switch_event_val=screen_switch_event_val)
         self.setup_mech_button()
+        self.setup_start_button()
         self.headlist = ["head1.png", "head2.png","clawdius.png"]
         self.bodylist = ["body1.png", "body2.png"]
         self.legslist = ["legs1.png", "legs2.png"]
@@ -14,6 +15,9 @@ class CreationScreen(Screen):
     def setup_mech_button(self):
         self.button_color = (0, 0, 128)  # Blue button
         self.button_rect = pygame.Rect(300, 300, 200, 50)  # Button dimensions and position
+    def handle_mech_button_event(self, event):
+        if self.button_rect.collidepoint(event.pos):
+            
     def setup_start_button(self):
         self.button_color = (0, 128, 0)  # Green button
         self.button_rect = pygame.Rect(300, 300, 200, 50)  # Button dimensions and position
